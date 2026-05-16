0653be6aab893a91370913409bc7ec2e306df699
runchenrun <runchenrun@runchenruns-MacBook-Air.local>
2026-05-15 21:57:41 -0700

    HW1, Task 1 to 3

--- Day 1/firstcprogram.c
+++ Day 1/firstcprogram.c
@@ -14,12 +14,14 @@
 int main() {
 
     int x;                          // x will be the value entered by the user
+    int y;                          // y will be the second value entered by the user
     char s[100];                    // s is used to "pause" the console so it doesn't exit too quickly
 
     /* Ask for a number and print it out */
     printf("Enter a number: ");     // prompt the user
     scanf("%d", &x);                // read a number from the console ("stdin")
     fgets(s,100,stdin);             // sadly, we have to remove the \n that scanf leaves in stdin
+
     printf("You entered %d\n", x);  // tell the user what number they just entered
     
     /* Normally I would not put a comment on every line of code.
@@ -27,7 +29,13 @@ int main() {
        I would only put a comment on a line if it was hard to understand or required that you
        know something that is not entirely obvious by looking at the code.
     */
-
+    printf("Enter the second number: ");
+    scanf("%d", &y);
     /* Pause the console so we can look at it before it vanishes */
     fgets(s,100,stdin);
+    printf("You entered %d\n", y);
+
+    printf("The sum of those two numbers is %d\n", x+y);
+
+    return 0;
 }
--- Day 1/helloworld.c
+++ Day 1/helloworld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    char s[100];
+    char s[100];  // Declare a charactor array named s that can hold up to 100 charactors
     printf("Hello world\n");
-    fgets(s, 100, stdin);
+    fgets(s, 100, stdin); // Read a line of input from the user
 }
