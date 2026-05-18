a71f36b34d9c2c4b35d2b33a39387ee30584f7a7
runchenrun <runchenrun@runchenruns-MacBook-Air.local>
2026-05-17 22:50:42 -0700

    Task 4 is done

--- Day 1/secondcprogram.c
+++ Day 1/secondcprogram.c
@@ -4,6 +4,10 @@ int main() {
     char t[100];
     printf ("What is your name? ");
     scanf("%s", s);
-    fgets(s,100,stdin);
-    printf("%s? That's a funny name!",t);
+    fgets(t,100,stdin); // I saved the leftover buffer into char t to get rid of the \n -> Not overrides the char s
+    printf("%s? That's a funny name!\n",s); // I added '\n' in the end of the string to leave space for the hint to exit the console
+    printf("Press the enter key to quit..."); // The hint for user to exit the console
+    getchar(); //Hit the Enter key to quit
+
+    return 0; //End the program with a return value of 0, which means the program ended successfully
 }
