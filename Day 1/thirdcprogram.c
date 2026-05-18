fbb71b3ce07013081e9d5707875ebcc8e9aaf6b7
runchenrun <runchenrun@runchenruns-MacBook-Air.local>
2026-05-17 23:00:47 -0700

    Unused comment removed

--- Day 1/thirdcprogram.c
+++ Day 1/thirdcprogram.c
@@ -1,11 +1,3 @@
-/* A First C Program - for Homework 1
- *
- * This code will start you off.  Modify it as instructed in Homework 1.
- * 
- * Put your name here: Professor Shafer
- * CS-5008 Spring 2025
- */
-
 /* Standard Libraries */
 #include <stdio.h>
 
@@ -24,12 +16,7 @@ int main() {
     fgets(s,100,stdin);             // sadly, we have to remove the \n that scanf leaves in stdin
 
     printf("You entered %d\n", x);  // tell the user what number they just entered
-    
-    /* Normally I would not put a comment on every line of code.
-       I just did it above because you are learning C for the first time.
-       I would only put a comment on a line if it was hard to understand or required that you
-       know something that is not entirely obvious by looking at the code.
-    */
+
     printf("Enter the second number: ");
     scanf("%d", &y);
     /* Pause the console so we can look at it before it vanishes */
